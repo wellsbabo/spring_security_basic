40fff67a709ff29a224585d0019f7f106466c8e2
wellsbabo <wellsbabo@naver.com>
2024-08-12 16:29:01 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+참고한 재생목록
+- https://youtube.com/playlist?list=PLJkjrxxiBSFCKD9TRKDYn7IE96K2u3C3U&si=ywprryUfy-SDyrCg
+
+정리된 문서 리스트
+- https://www.devyummi.com/page?id=668bd2d92b88a1ef5f2be2e3
---
+++ src/main/java/wellsbabo/spring_security_basic/SpringSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package wellsbabo.spring_security_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicApplication.class, args);
+    }
+
+}
